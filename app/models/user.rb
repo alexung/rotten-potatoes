94bb5559600042cf9261b3153b9ec0cab78a0a95
Alex Ung <alexung.ucsb@gmail.com>
2014-10-01 00:52:52 -0400

    Added rudimentary user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
 	has_secure_password
 
+	def self.authenticate(username, password)
+	    user = User.find_by(username: username)
+	    return user.authenticate(password) unless user == nil
+	    nil
+ 	end
 end
